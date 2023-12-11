6fdaca2b19befa1cd438d0eb948ea0dd71056245
Carla Aguiar <carlacristec.gois@gmail.com>
2023-12-11 17:35:56 -0300

    Atividade3 commit

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,27 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("ID=sa;Password=admin;Server=localhost;Database=ExoApi;- + Trusted_Connection=False;");
+        }
+
+    }
+        public DbSet<Projeto> Projetos {get; set; }
+    }
+
+}
