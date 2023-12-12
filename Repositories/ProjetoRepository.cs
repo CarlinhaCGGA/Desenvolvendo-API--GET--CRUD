4d36f6413a7be372da237b78060413e4fcebdfa3
Carla Aguiar <carlacristina.gois@gmail.com>
2023-12-12 01:24:15 -0300

    Update ProjetoRepository.cs

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -17,6 +17,34 @@ namespace Exo.WebApi.Repositories
         public List<Projeto> Listar()
         {
         return _context.Projetos.ToList();
-        }   
+            public void Cadastrar(Projeto projeto)
+        {
+        _context.Projetos.Add(projeto);
+        _context.SaveChanges();
+        }
+        public Projeto BuscarporId(int id)
+        {
+        return _context.Projetos.Find(id);
+        }
+        public void Atualizar(int id, Projeto projeto)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            if (projetoBuscado != null)
+            {
+            projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+            projetoBuscado.Area = projeto.Area;
+            projetoBuscado.Status = projeto.Status;
+            }
+            _context.Projetos.Update(projetoBuscado);
+            _context.SaveChanges();
+        }
+        public void Deletar(int id)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            _context.Projetos.Remove(projetoBuscado);
+            _context.SaveChanges();
+        
+    
+       }   
     }
 }
