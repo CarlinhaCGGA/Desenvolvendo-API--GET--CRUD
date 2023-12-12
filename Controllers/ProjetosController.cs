750f9b3c55b72ad91b3ace0cb94bbb5d3a23547e
Carla Aguiar <carlacristina.gois@gmail.com>
2023-12-12 01:27:28 -0300

    Update ProjetosController.cs

--- Controllers/ProjetosController.cs
+++ Controllers/ProjetosController.cs
@@ -20,6 +20,39 @@ namespace Exo.WebApi.Controllers
         public IActionResult Listar()
         {
         return Ok(_projetoRepository.Listar());
+         [HttpPost]
+        public IActionResult Cadastrar(Projeto projeto)
+        {
+            _projetoRepository.Cadastrar(projeto);
+            return StatusCode(201);
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Projeto projeto = _projetoRepository.BuscarporId(id);
+            if (projeto == null)
+            {
+            return NotFound();
+            }
+            return Ok(projeto);
+         }
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Projeto projeto)
+        {
+            _projetoRepository.Atualizar(id, projeto);
+            return StatusCode(204);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _projetoRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+            return BadRequest();
         }
     }
 }
